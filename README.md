3a5accbf8bc0f0eb8e9fe70a7c2313ae058a5673
Timo Meyer <timomey@gmail.com>
2015-11-13 12:29:29 -0800

    notes
    wrote some notes.

---
+++ README.md
@@ -0,0 +1,2 @@
+TODO: write this readme -> what is needed to run the programs. python version and imports and stuff.
+make run.sh
--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 # example of the run script for running the word count
+#still do that stuff here; make both programs nicer and make sure they take the input. 
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
 python ./src/words_tweeted.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
 python ./src/median_unique.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
-
-
-
